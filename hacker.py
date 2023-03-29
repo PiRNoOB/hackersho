afe3341a139a9763e043749ef4417918c34bc9c3
PiRNoOB <66233562+PiRNoOB@users.noreply.github.com>
2023-03-29 20:01:48 +0330

    Add files via upload

---
+++ hacker.py
@@ -0,0 +1,35 @@
+import os 
+import time 
+print ("sakhete shode tvsot : ")
+time.sleep(1)
+print("script by Aref Ramezani")
+print("instagram : aref.love.ricin")
+print("Telgram : @ArEFKHaS")
+time.sleep(1)
+print()
+print("Az Control+C Bray Tamam Kardan HACK Estefade Konid(Android)")
+print("Az Control+x Bray Tamam Kardan HACK Estefade Konid(pc)")
+time.sleep(1)
+print("[Targets] Mi Tavan Ba Gmail , shomare va ip Estefade Kard")
+target=input("target :")
+time.sleep(2)
+print("Connecting To " + target)
+time.sleep(1)
+print("Start Hacking")
+time.sleep(3)
+i=2
+ while>100000
+ if not(i%j):break
+ j=j+1
+ if(j>i/j)print(i,"Ersal virus to "+target)
+ time.sleep(1)
+ print("target :"+target)
+ print("status : HACKED!" )
+ print("virus ersal shode : 383295")
+time.sleep(2)
+print("system hack shode digar dar dasters nist")
+print("bahtare massege ha pak shan ta gir niofti :)")
+time.sleep(3)
+print("goodbye my friends")
+time.sleep(2)
+os.system("clear")
