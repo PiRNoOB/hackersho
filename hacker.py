64865ba3beb8aa9a4b08c0942f2d81f63689a21b
PiRNoOB <66233562+PiRNoOB@users.noreply.github.com>
2023-03-29 20:34:24 +0330

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -1,6 +1,7 @@
-import os 
-import time 
-print ("sakhete shode tvsot : ")
+import os
+import time
+#BATAS GAN
+print("sakhete shode tvsot : ")
 time.sleep(1)
 print("script by Aref Ramezani")
 print("instagram : aref.love.ricin")
@@ -10,28 +11,31 @@ print()
 print("Az Control+C Bray Tamam Kardan HACK Estefade Konid(Android)")
 print("Az Control+x Bray Tamam Kardan HACK Estefade Konid(pc)")
 time.sleep(1)
-print("[Targets] Mi Tavan Ba Gmail , shomare va ip Estefade Kard")
-target=input("target :")
+print("[Targets] - Mi Tavan Ba Gmail , shomare va ip Estefade Kard")
+target = input("Target : ")
 time.sleep(2)
-print("Connecting To " + target)
+print("CONNECTING TO " + target)
+time.sleep(1)
+print("Amade Sazy Abzar ha")
 time.sleep(1)
 print("Start Hacking")
 time.sleep(3)
+i = 2
 while(i < 1000000):
    j = 2
    while(j <= (i/j)):
       if not(i%j): break
       j = j + 1
-   if (j > i/j) : print (i, "Aresal VIRUS TO " + target)
+   if (j > i/j) : print (i, "Ersal VIRUS TO " + target)
    i = i + 1
- time.sleep(1)
- print("target :"+target)
- print("status : HACKED!" )
- print("virus ersal shode : 383295")
+time.sleep(1)
+print("TARGET : " + target)
+print("STATUS : Hacked!")
+print("virus ersal shode : 383295")
 time.sleep(2)
-print("system hack shode digar dar dasters nist")
+print("system hack shode digar dar dasters nist!")
 print("bahtare massege ha pak shan ta gir niofti :)")
-time.sleep(3)
-print("goodbye my friends")
-time.sleep(2)
+time.sleep(1)
+print("goodbye my friends!")
+time.sleep(1)
 os.system("clear")
